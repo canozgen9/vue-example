e04ad84165628397459236b59710616107001032
canozgen9 <canozgen9@hotmail.com>
2018-12-22 06:21:32 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,64 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+    <div id="app">
+        <!--<Header ref="header" title="Can" v-on:titleChanged="titleChanged"></Header>-->
+        <!--<input type="text" v-model="$store.state.username">-->
+        <button v-on:click="changeHeaderTitle">Select</button>
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+        <div v-if="datas !== null">
+
+            <ul>
+                <li v-for="(data, i) in datas">
+                    {{i}}.item {{data.title}}
+                </li>
+            </ul>
+
+        </div>
+
+        <div v-else>
+            Loading...
+        </div>
+
+    </div>
+</template>
+
+<style lang="stylus">
+
 </style>
+
+<script>
+    import Header from './components/Header'
+
+    export default {
+        name: 'App',
+        data() {
+            return {
+                inputText: '',
+                datas: null
+            }
+        },
+        methods: {
+            changeHeaderTitle() {
+                this.datas.splice(0, 30);
+            },
+            titleChanged(title) {
+                alert(title);
+            }
+        },
+        async mounted() {
+            try {
+                let result = await this.$axios.post('https://jsonplaceholder.typicode.com/todos', {
+                    ilker: 'sa'
+                });
+                setTimeout(() => {
+                    this.datas = result.data;
+                }, 1000);
+            } catch (e) {
+                console.log(e);
+            }
+        },
+        components: {
+            Header
+        }
+    }
+</script>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="header">
+        {{$store.state.username}}
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+
+<style lang="stylus">
+    .header
+        padding 20px
+        background-color #F00
+        color #FFF
+</style>
+
+<script>
+    export default {
+        name: 'Header',
+        props: [
+            'title'
+        ],
+        methods: {
+            changeTitle(title) {
+                this.title = title;
+
+                setTimeout(() => {
+                    this.$emit('titleChanged', title);
+                }, 1000);
+            }
+        },
+        computed: {
+            username() {
+                return this.$store.state.username;
+            }
+        }
+    }
+</script>
